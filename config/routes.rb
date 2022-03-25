41ed268d4c0d359d73420375c2344d57d347ec70
od-c0d3r <omar_rashad@aol.com>
2022-03-25 14:53:23 +0200

    Routes: setup reservations API endpoints routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 , defaults: { format: 'json'} do
       resources :items, only: %i[index create destroy show]
+      resources :reservations, only: %i[index create destroy]
     end
   end
   # Defines the root path route ("/")
