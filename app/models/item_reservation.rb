3243a14e746d13a10865d61969118f73468fea65
MCKENT05 <atemitope95@gmail.com>
2022-03-25 22:40:57 +0100

    set validations for reservation and item_reservation models

--- app/models/item_reservation.rb
+++ app/models/item_reservation.rb
@@ -1,2 +1,4 @@
 class ItemReservation < ApplicationRecord
+    belongs_to :reservation, class_name: "Reservation", foreign_key: "reservation_id"
+    belongs_to :item, class_name: "Item", foreign_key: "item_id"
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,2 +1,7 @@
 class Reservation < ApplicationRecord
+    has_many :itemreservations, class_name: "ItemReservation", foreign_key: "reservation_id", dependent: :destroy
+    belongs_to :user, class_name: "User", foreign_key: "user_id"
+
+    validates :start_date, presence: true
+    validates :end_date, presence: true
 end
