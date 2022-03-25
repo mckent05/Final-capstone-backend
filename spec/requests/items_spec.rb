23e83f2b42bf8d9d1ced48b286a8d24e77ced3a7
MCKENT05 <atemitope95@gmail.com>
2022-03-25 15:15:18 +0100

    Finished setting up routes, controllers and models according to ERD

--- spec/requests/items_spec.rb
+++ spec/requests/items_spec.rb
@@ -1,32 +1,31 @@
 require 'rails_helper'
 
-RSpec.describe "Items", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/items/index"
+RSpec.describe 'Items', type: :request do
+  describe 'GET /index' do
+    it 'returns http success' do
+      get '/items/index'
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /create" do
-    it "returns http success" do
-      get "/items/create"
+  describe 'GET /create' do
+    it 'returns http success' do
+      get '/items/create'
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /destroy" do
-    it "returns http success" do
-      get "/items/destroy"
+  describe 'GET /destroy' do
+    it 'returns http success' do
+      get '/items/destroy'
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /show" do
-    it "returns http success" do
-      get "/items/show"
+  describe 'GET /show' do
+    it 'returns http success' do
+      get '/items/show'
       expect(response).to have_http_status(:success)
     end
   end
-
 end
