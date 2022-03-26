d8232fcc54f26ee643ed1d12eb94acb703ba21bc
MCKENT05 <atemitope95@gmail.com>
2022-03-26 20:40:18 +0100

    Add logic to reservations controller

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -1,7 +1,36 @@
 class Api::V1::ReservationsController < ApplicationController
-  def index; end
+  def index
+    reservations = User.first.reservations.includes(:item_reservations)
+    list = []
+    reservations.each do |reservation|
+      reservation.item_reservations.each { |item_reserve| list << { reservation: reservation.as_json(only: [:id, :start_date, :end_date]), item: item_reserve.item.as_json(only: [:id, :name, :capacity, :price, :city, :description, :image]) }}
+    end
+    render json: { data: list.as_json, status: 200 }
+  end
 
-  def create; end
+  def create
+    reserved_item = Item.find(params[:item_id])
+    reservation_made = User.first.reservations.new(start_date: params[:start_date], end_date: params[:end_date])
+    item_reservation = ItemReservation.new(reservation: reservation_made, item: reserved_item)
+    if item_reservation.save
+      render json: { message: "Reservation for #{reserved_item.name} made succesfully", status: 200 }
+    else
+      render json: { message: "Reservation for #{reserved_item.name} failed", status: 400 }
+    end
+  end
 
-  def destroy; end
+  def destroy
+    reservation = Reservation.find(params[:id])
+    if reservation.destroy
+      render json: { message: 'Reservation succesfully deleted', status: 200 }
+    else
+      render json: { message: 'Reservation not deleted', status: 400 }
+    end
+  end
+
+  # private
+
+  # def new_reservation_params
+  #   params.require(:reservation).permit(:start_date, :end_date)
+  # end
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,7 +1,8 @@
 class Reservation < ApplicationRecord
-    has_many :itemreservations, class_name: "ItemReservation", foreign_key: "reservation_id", dependent: :destroy
-    belongs_to :user, class_name: "User", foreign_key: "user_id"
+  has_many :item_reservations, class_name: 'ItemReservation', foreign_key: 'reservation_id', dependent: :destroy
+  has_many :items, through: :item_reservations, dependent: :destroy
+  belongs_to :user, class_name: 'User', foreign_key: 'user_id'
 
-    validates :start_date, presence: true
-    validates :end_date, presence: true
+  validates :start_date, presence: true
+  validates :end_date, presence: true
 end
