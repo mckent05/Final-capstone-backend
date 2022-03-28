fe1ea47cabf373c244d3b0c32847acab460fa8cd
od-c0d3r <omar_rashad@aol.com>
2022-03-28 16:24:51 +0200

    Controller: create items controller actions

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,9 +1,30 @@
 class Api::V1::ItemsController < ApplicationController
-  def index; end
+  def index
+    items = Item.all
+    render json: items
+  end
 
-  def create; end
+  def create
+    user = User.find(params[:user_id])
+    new_item = user.items.new(items_params)
+    if new_item.save
+      render json: { message: "Item created succesfully", status: 200 }
+    else
+      render json: { message: "Item failed to create", status: 400 }
+    end
+  end
 
-  def destroy; end
+  def destroy
+    item = Item.find(params[:id])
+    if item.destroy
+      render json: { message: 'Item succesfully deleted', status: 200 }
+    else
+      render json: { message: 'Item failed to deleted', status: 400 }
+    end
+  end
 
-  def show; end
+  def show
+    item = Item.find(params[:id])
+    render json: item
+  end
 end
