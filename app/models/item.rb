0f685aff2388369be1f6e0835b0ee276d16beb7e
od-c0d3r <omar_rashad@aol.com>
2022-03-28 14:44:15 +0200

    Fix: item model typo

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
   has_many :item_reservations, class_name: 'ItemReservation', foreign_key: 'item_id', dependent: :destroy
-  has_many :reservations, through: :itemreservations, dependent: :destroy
+  has_many :reservations, through: :item_reservations, dependent: :destroy
   belongs_to :user, class_name: 'User', foreign_key: 'user_id'
 
   validates :name, presence: true, uniquenes: true
