1c3a362694bc2681bb4acc546390c19437259d74
MCKENT05 <atemitope95@gmail.com>
2022-03-28 13:08:15 +0100

    Fixed issues with secure parameters syntax

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,7 @@ class Item < ApplicationRecord
   has_many :reservations, through: :itemreservations, dependent: :destroy
   belongs_to :user, class_name: 'User', foreign_key: 'user_id'
 
-  validates :name, presence: true, uniquenes: true
+  validates :name, presence: true, uniqueness: true
   validates :description, presence: true, length: { minium: 50, maximum: 100 }
   validates :price, presence: true, numericality: { only_integer: true }
   validates :city, presence: true
