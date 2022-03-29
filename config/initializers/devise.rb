c73979eb6ae12b2db9b5d898ae8529aefae3dfd6
od-c0d3r <omar_rashad@aol.com>
2022-03-29 10:50:29 +0200

    Session: create sessions controller

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -11,7 +11,8 @@
 Devise.setup do |config|
   
   config.jwt do |jwt|
-    jwt.secret = ENV['DEVISE_JWT_SECRET_KEY']    jwt.dispatch_requests = [
+    jwt.secret = ENV['DEVISE_JWT_SECRET_KEY']
+    jwt.dispatch_requests = [
       ['POST', %r{^/sign_in$}]
     ]
     jwt.revocation_requests = [
