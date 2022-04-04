624ecc58d094cae314173091a6affe3465b4958c
Omar R <omar_rashad@aol.com>
2022-04-04 16:59:52 +0200

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
 
+admin = User.create!(username:"admin") 
 
 10.times do
     Item.create!(
